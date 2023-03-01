a274ba224b11d70e4e51defdf7ac751aabdfbc4d
ranelho <ranelhol@gmail.com>
2023-03-01 11:33:04 -0300

    implementa busca cliente por id

--- clientes/src/main/java/com/rlti/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/rlti/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import com.rlti.clientes.model.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @AllArgsConstructor
@@ -19,5 +20,10 @@ public class ClienteController {
         return repository.save(cliente);
     }
 
-
+    @GetMapping("/{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
